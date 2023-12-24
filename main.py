b6e3fcd43d72af4d75550f6b0274f0d4938294e8
Klayton Pagel <pagelk@lopers.unk.edu>
2023-12-23 21:20:39 -0600

    add rows to a table
    started formatting the file to use functions for each type of query. added the use of variables to make it easy to add rows to desired table.

--- main.py
+++ main.py
@@ -9,7 +9,31 @@ testdb = mysql.connector.connect(
 
 dbcursor = testdb.cursor()
 
-dbcursor.execute("SHOW TABLES")
 
-for table in dbcursor:
-    print(table)
+# show tables in the database
+def show_tables():
+    dbcursor.execute("SHOW TABLES")
+
+    for table in dbcursor:
+        print(table)
+
+
+# show all columns in a table
+def show_columns(table):
+    query = "SELECT * FROM {}"
+    query = query.format(table)
+    dbcursor.execute(query)
+
+    for column in dbcursor:
+        print(column)
+
+# Adds rows to your database
+def add_rows(table, values):
+    query = "INSERT INTO {} (username, password) VALUES (%s, %s)"
+    query = query.format(table)
+    dbcursor.execute(query, values)
+
+    testdb.commit()
+
+
+show_columns('login_credentials')
